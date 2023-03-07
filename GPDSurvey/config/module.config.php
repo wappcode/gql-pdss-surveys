00150d4509143f80bdbc7e46163a501f08866f2f
Jesus Abarca <jesus.abarca.g@gmail.com>
2023-03-07 14:09:39 -0600

    Se actualizo el controlador para subir archivos y se agregaron comentarios y referencias en la configuración para este controlador

--- GPDSurvey/config/module.config.php
+++ GPDSurvey/config/module.config.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'survey_upload_file_dir' => 'survey_files'
+    'survey_upload_file_dir' => 'survey_files',
+    'app_upload_dir' => null // sobreescribir en la configuración de app module con la ruta del directorio para subir archivos ejemplo public/uploads
 ];
--- GPDSurvey/src/Controller/SurveyUploadFileController.php
+++ GPDSurvey/src/Controller/SurveyUploadFileController.php
@@ -2,6 +2,7 @@
 
 namespace GPDSurvey\Controller;
 
+use Exception;
 use GPDCore\Library\AbstractAppController;
 use GPDSurvey\Services\SurveyUploadFileService;
 
@@ -17,6 +18,9 @@ class SurveyUploadFileController extends AbstractAppController
 
         $dir = $this->context->getConfig()->get('survey_upload_file_dir');
         $uploadDir = $this->context->getConfig()->get("app_upload_dir");
+        if (empty($dir) || empty($uploadDir)) {
+            throw new Exception("Internal server error", 500);
+        }
         SurveyUploadFileService::upload($this->context, $uploadDir, $dir);
     }
 }
