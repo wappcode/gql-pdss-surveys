c8b5675a860eff39d3fad365665238273ab988e8
JJesus Abarca González <j.abarca@qualem.com>
2025-10-13 17:49:32 -0600

    Corrección para dar formato con los labels de las opciones a una respuesta

--- GPDSurvey/src/Library/QuestionOptionsValueUtilities.php
+++ GPDSurvey/src/Library/QuestionOptionsValueUtilities.php
@@ -7,12 +7,21 @@ class QuestionOptionsValueUtilities
 {
 
 
-    public static function getFormatedAnswer(array $question, array $options, ?string $answer): ?string
+    /**
+     * Formatea una respuesta utilizando las opciones de la pregunta y aplica un formato especial para imagenes y archivos
+     *
+     * @param array $question array con los nombres de las columnas de la base como clave ["question_type"] o ["type"]
+     * @param array $options Opciones de la pregunta
+     * @param string|null $answer
+     * @param string|null $fileUrlBase (url para descargar o ver archivo al final de la url se agrega la respuesta https://demo.com/updloads/{respuesta})
+     * @return string|null
+     */
+    public static function getFormatedAnswer(array $question, array $options, ?string $answer, ?string $fileUrlBase = null): ?string
     {
         if (empty($answer)) {
             return $answer;
         }
-        $type = $question["type"];
+        $type = $question["type"] ?? $question["question_type"];
         $formatedAnswer = $answer;
         switch ($type) {
             case ISurveyQuestion::SURVEY_QUESTION_TYPE_CHECKBOX_LIST:
@@ -21,7 +30,7 @@ class QuestionOptionsValueUtilities
                 break;
             case ISurveyQuestion::SURVEY_QUESTION_TYPE_IMAGE:
             case ISurveyQuestion::SURVEY_QUESTION_TYPE_FILE:
-                $formatedAnswer = static::formatQuestionFile($answer);
+                $formatedAnswer = static::formatQuestionFile($answer, $fileUrlBase);
                 break;
         }
 
@@ -35,10 +44,13 @@ class QuestionOptionsValueUtilities
         return $formatedValue;
     }
 
-    protected static function formatQuestionFile(string $answer): string
+    protected static function formatQuestionFile(string $answer, ?string $fileUrlBase = null): string
     {
+        if (!is_string($fileUrlBase) || empty($fileUrlBase)) {
+            $fileUrlBase = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . DIRECTORY_SEPARATOR . "uploads";
+        }
         if (!empty($answer)) {
-            $url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR . $answer;
+            $url = $fileUrlBase . DIRECTORY_SEPARATOR . $answer;
             return  $url;
         } else {
             return "";
